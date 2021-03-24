e63a2f734daec93aa5df44198b5752b0084d68bf
MWANZIA13 <77730777+MWANZIA13@users.noreply.github.com>
2021-03-24 16:21:10 +0300

    Add files via upload

---
+++ list/list.py
@@ -0,0 +1,27 @@
+Names=["Dennis","Isaac","Ferdinand","Jane","Beniter"]
+print(Names)
+
+clubs=list(("Barcelona","Dortmund"))
+print(clubs)
+
+list3=[True, False, False]
+print(list3)
+
+fruits=["apple","banana","cherry"]
+print(fruits[-3])
+print(fruits[0:2])
+print(Names[3:5])
+
+fruits=["apple","banana","cherry"]
+if "apple" in fruits:
+  print("Yes,'apple' is in the fruits list")
+if "mango" in fruits:
+  print("Yes,'mango' is in the fruits list") 
+    
+players=["Messi","Ronaldo","Neymar","Mbappe"]
+players[1]="Haaland" #change the second item
+print(players)
+
+colours=["White","Blue","Red","Green"]
+colours.append("orange")
+print(colours)
